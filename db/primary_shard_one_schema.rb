ff37d15eb657f2c54e0995bedcab4aee5225d85f
Daniel Torres <denial.torres@gmail.com>
2022-09-25 17:35:24 -0500

    include Person model and seeds

--- db/primary_shard_one_schema.rb
+++ db/primary_shard_one_schema.rb
@@ -10,5 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2022_09_25_223103) do
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Person.create!(
+  name: 'Frieda'
+)
+
+Person.create!(
+  name: 'Bill'
+)
+
+Person.create!(
+  name: 'Penelope'
+)
